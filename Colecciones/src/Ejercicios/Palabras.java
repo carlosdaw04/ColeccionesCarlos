362ff82aa48c4ad687e762261f2144779ddf89d9
Carlos <costales98@gmail.com>
2019-02-13 14:22:33 +0100

    Ejercicio 3 solucionado en clase.

--- Colecciones/src/Ejercicios/Palabras.java
+++ Colecciones/src/Ejercicios/Palabras.java
@@ -1,23 +1,59 @@
 package Ejercicios;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Palabras {
-
-	String cadena;
-	public Palabras(String cadena) {
-		this.cadena = cadena;
-	}
 	
-	public ArrayList<String> palabrasNoRepetidas(String cadena) {
-		ArrayList<String> pl = new ArrayList<String>();
-		String[] cadenapartida = cadena.split(" ");
-		
-		for(int i=0; i<cadenapartida.length; i++) {
-			if(!cadenapartida[i].equals(pl)) {
-				pl.add(cadenapartida[i]);
+	Set<String> sinRepetir = new HashSet<String>();
+	Set<String> repetidas = new HashSet<String>();
+	
+	public Palabras(String s) {
+		String palabra = "";
+		for(int i=0; i<s.length(); i++) {
+			char c = s.toLowerCase().charAt(i);
+			if(Character.isAlphabetic(c)) {
+				palabra+= c;
+				if(i == s.length()-1 || !Character.isAlphabetic(s.charAt(i+1))) {
+					if(sinRepetir.contains(palabra)) {
+						repetidas.add(palabra);
+						sinRepetir.remove(palabra);
+					}else {
+						sinRepetir.add(palabra);
+					}
+					palabra = "";
+				}
 			}
 		}
-		return pl;
+	}
+	
+	public Set<String> getSinRepetir() {
+		return sinRepetir;
+	}
+	
+	public Set<String> getRepetidas() {
+		return repetidas;
+	}
+
+	public static void main(String[] args) {
+		Palabras p = new Palabras("Esto es una prueba es");
+		System.out.println("Sin repetir: ");
+		mostrarColeccion(p.getSinRepetir());
+		System.out.println("Repetidas: ");
+		mostrarColeccion(p.getRepetidas());
+	}
+	
+	static void mostrarColeccion(Collection<String> c) {
+		System.out.println(c.getClass().getSimpleName() + ": " + c.size() + " elementos");
+		Iterator<String> i = c.iterator();
+		while (i.hasNext()) {
+			String s = i.next();
+			System.out.print(s);
+			if (i.hasNext())
+				System.out.print(" ");
+		}
+		System.out.println();
 	}
 }
